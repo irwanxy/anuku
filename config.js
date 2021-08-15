c727ae366fcd5a6e0c91b4ae021f906cb1636ed1
irwanx <51849384+irwanx@users.noreply.github.com>
2021-08-15 17:29:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ global.APIs = { // API Prefix
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
-  'https://api.xteam.xyz': 'd90a9e986e18778b',
+  'https://api.xteam.xyz': '4e1ca5d1ef6963ef',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
   'https://api.zeks.xyz': 'apivinz',
   'https://pencarikode.xyz': 'pais',
